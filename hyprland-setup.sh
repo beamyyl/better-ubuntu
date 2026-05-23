6059d3b99cd33b8543f2993ec65d23698c6695ee
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-05-23 18:18:31 +0300

    Update hyprland-setup.sh

--- hyprland-setup.sh
+++ hyprland-setup.sh
@@ -1,19 +1,20 @@
 echo "How would you like to setup Hyprland?"
-echo "1) Build from source (compiles latest Hyprland, but for new updates you have to re-run this script)"
-echo "2) Install the binary from Ubuntu's repo (might be outdated)"
+echo "1) Install the binary from Ubuntu's repo (might be a bit outdated)"
+echo "2) Build from source (NOT RECOMMENDED! Compiles latest Hyprland with a custom script (not made by me), but for new updates you have to re-run this script, and it MIGHT NOT WORK!)"
+
 while true; do
     read -p "Enter your choice (1 or 2): " choice
     case $choice in
         1 ) 
-            git clone https://gitlab.com/kralos/hyprbuntu
-            cd hyprbuntu/
-            ./setup-hyprbuntu.sh
-            break
-            ;;
-        2 ) 
             echo "Installing Hyprland binary via apt..."
             sudo apt update && sudo apt install -y hyprland
             echo "You can now start Hyprland from GDM, or install sddm"
+            break
+            ;;
+        2 ) 
+            git clone https://gitlab.com/kralos/hyprbuntu
+            cd hyprbuntu/
+            ./setup-hyprbuntu.sh
             break
             ;;
         * ) 
