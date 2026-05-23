82344e36e330c67579aece82d1bab83523abbbf8
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-05-23 17:34:51 +0300

    Update xfce-setup.sh

--- xfce-setup.sh
+++ xfce-setup.sh
@@ -1,4 +1,6 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install xfce4 xfce4-goodies xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings pavucontrol -y
+sudo mkdir /usr/share/xfce4/backdrops -p
+sudo cp -r /usr/share/backgrounds/* /usr/share/xfce4/backdrops/
 echo "You can now reboot to go into Xfce"
