648ffc20490250fba6a44140aeae7e6b6667077b
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-05-23 17:39:46 +0300

    Update kde-setup.sh

--- kde-setup.sh
+++ kde-setup.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install kde-plasma-desktop sddm-theme-breeze konsole dolphin -y
+sudo apt install kde-plasma-desktop sddm-theme-breeze breeze-gtk-theme breeze-icon-theme konsole dolphin -y
 sudo mkdir -p /etc/sddm.conf.d
 sudo mv mv /etc/sddm.conf.d/50-ubuntu-budgie.conf /etc/sddm.conf.d/50-ubuntu-budgie.bak
 echo -e "[Theme]\nCurrent=breeze" | sudo tee /etc/sddm.conf.d/kde_settings.conf
