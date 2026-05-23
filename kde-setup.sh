8ad9568fcd5d81a958d7baa17c88f9c1287547b7
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-05-23 19:38:32 +0300

    Update kde-setup.sh

--- kde-setup.sh
+++ kde-setup.sh
@@ -3,7 +3,7 @@ sudo apt upgrade -y
 sudo apt install kde-plasma-desktop sddm-theme-breeze breeze-gtk-theme breeze-icon-theme konsole dolphin kde-spectacle -y
 sudo dpkg-reconfigure sddm
 sudo mkdir -p /etc/sddm.conf.d
-sudo mv mv /etc/sddm.conf.d/50-ubuntu-budgie.conf /etc/sddm.conf.d/50-ubuntu-budgie.bak
+sudo mv /etc/sddm.conf.d/50-ubuntu-budgie.conf /etc/sddm.conf.d/50-ubuntu-budgie.bak
 echo -e "[Theme]\nCurrent=breeze" | sudo tee /etc/sddm.conf.d/kde_settings.conf
 sudo apt purge light-locker -y
 echo "You can now reboot to go into plasma"
