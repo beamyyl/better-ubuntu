9c077e338942f7be0254b37e60837f339378d2b2
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-05-23 18:18:40 +0300

    Update hyprland-setup.sh

--- hyprland-setup.sh
+++ hyprland-setup.sh
@@ -6,7 +6,6 @@ while true; do
     read -p "Enter your choice (1 or 2): " choice
     case $choice in
         1 ) 
-            echo "Installing Hyprland binary via apt..."
             sudo apt update && sudo apt install -y hyprland
             echo "You can now start Hyprland from GDM, or install sddm"
             break
