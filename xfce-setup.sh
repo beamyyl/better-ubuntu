1f5692a5136989813816be6104e9424181a330bc
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-05-23 17:21:31 +0300

    Update xfce-setup.sh

--- xfce-setup.sh
+++ xfce-setup.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install xfce4 xfce4-goodies xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin pavucontrol -y
+sudo apt install xfce4 xfce4-goodies xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings pavucontrol -y
 echo "You can now reboot to go into Xfce"
