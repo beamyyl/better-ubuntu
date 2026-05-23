85570468401d057d6f21f54644e932c6915ff4c3
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-05-23 17:41:26 +0300

    Update cinnamon-setup.sh

--- cinnamon-setup.sh
+++ cinnamon-setup.sh
@@ -1,4 +1,5 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install cinnamon-core lightdm slick-greeter lightdm-settings -y
+sudo dpkg-reconfigure lightdm
 echo "You can now reboot to go into Cinnamon"
