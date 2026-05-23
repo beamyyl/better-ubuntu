7bb34faf20ee84a4e28f9f6aa41cfe328f3b9581
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-05-23 17:51:00 +0300

    Update kde-setup.sh

--- kde-setup.sh
+++ kde-setup.sh
@@ -5,4 +5,5 @@ sudo dpkg-reconfigure sddm
 sudo mkdir -p /etc/sddm.conf.d
 sudo mv mv /etc/sddm.conf.d/50-ubuntu-budgie.conf /etc/sddm.conf.d/50-ubuntu-budgie.bak
 echo -e "[Theme]\nCurrent=breeze" | sudo tee /etc/sddm.conf.d/kde_settings.conf
+sudo apt purge light-locker -y
 echo "You can now reboot to go into plasma"
