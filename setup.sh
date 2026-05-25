046d669132c8c778633ecf8665c034e61cd93e16
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-05-25 22:29:40 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo dpkg --add-architecture i386
 
 sudo apt update
 sudo apt install flatpak gnome-software-plugin-flatpak -y
-flatpak remote-add --if-not-exists flathub https://flathub.org
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 sudo systemctl stop snapd.service snapd.socket 2>/dev/null
 
