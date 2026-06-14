1d0dd85f32b937b9515e90802be1de063247689e
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-06-14 19:24:52 +0300

    Update servertonormal-setup.sh

--- servertonormal-setup.sh
+++ servertonormal-setup.sh
@@ -4,13 +4,19 @@ sudo rm -rf /etc/cloud/ /var/lib/cloud/
 sudo apt purge multipath-tools
 sudo apt autoremove --purge
 sudo apt install -y network-manager iputils-ping net-tools dnsutils curl
-echo -e "network:\n  version: 2\n  renderer: NetworkManager" | sudo tee /etc/netplan/01-networkmanager.yaml
-sudo rm -f /etc/netplan/00-installer-config.yaml /etc/netplan/50-cloud-init.yaml
-sudo netplan apply
-sudo systemctl enable NetworkManager --now
-sudo systemctl disable systemd-networkd.service --now
-sudo systemctl mask systemd-networkd-wait-online.service
-sudo touch /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
-#sudo sed -i 's/managed=false/managed=true/g' /etc/NetworkManager/NetworkManager.conf
-sudo systemctl restart NetworkManager
+
+cd /etc/netplan
+cp 01-netcfg.yaml 01-netcfg.yaml.BAK
+
+cat << EOF > /etc/netplan/01-netcfg.yaml
+network:
+  version: 2
+  renderer: NetworkManager
+EOF
+
+netplan generate
+netplan apply
+systemctl enable NetworkManager.service
+systemctl restart NetworkManager.service
+
 sudo reboot
